bc7eac0b4c7bb6e34d1e01ff42e79e28ba39d7ba
ithlp <per@ithlp.dk>
2021-07-31 13:47:30 +0200

    Add files via upload

---
+++ Win10_20H2.ps1
@@ -0,0 +1,63 @@
+#=======================================================================
+#   OS: Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSBuild = "21H1"
+    OSEdition = "Enterprise"
+    OSLanguage = "da-dk"
+    OSLicense = "Retail"
+    SkipAutopilot = $true
+    SkipODT = $true
+}
+
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
+
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+
+if ((Get-MyComputerModel) -match 'Virtual') {
+
+    Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
+
+    Set-DisRes 1600
+
+}
+
+#Make sure I have the latest OSD Content
+
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+
+Install-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
+
+Import-Module OSD -Force
+
+Start-OSDCloud @Params
+
+#=======================================================================
+#   PostOS: AutopilotOOBE Staging
+#=======================================================================
+$AutopilotOOBEJson = @'
+{
+    "Assign":  {
+                   "IsPresent":  true
+               },
+    "GroupTag":  "Enterprise"
+    "GroupTagOptions":  [
+                            "Development",
+                            "Enterprise"
+                        ],
+    "Hidden":  [
+                   "AddToGroup",
+                   "AssignedComputerName",
+                   "PostAction"
+               ],
+    "PostAction":  "Quit",
+    "Run":  "NetworkingWireless",
+    "Docs":  "https://autopilotoobe.osdeploy.com/",
+    "Title":  "OSDeploy Autopilot Registration"
+}
+'@
+$AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json" -Encoding ascii -Force
